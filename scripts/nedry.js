0fcaa1a3b89a3b48a7cf54a9f6351bec46b3a105
Henry Mesias <henrymesias@gmail.com>
2015-05-21 20:26:39 -0400

    sound added. Image fades in

--- scripts/nedry.js
+++ scripts/nedry.js
@@ -1,36 +1,68 @@
 
+	
+
 
 $.fn.wrongAnswer = function(){
 
-// $('submitButton').submit(function(e){
-// 		e.preventDefault();
-
 	
-	this.on('submit', function(e){
+
+	$("#submit").on('click', function(e){
 		e.preventDefault();
+		// Prevent the default function
+
+
+var nedryImageMarkup = '<img id="dNedry" style="display: none" src="images/nedryLarge.png" />'
+			
+	var input = $("#searchAudio").val();
+	// create variable to hold value entered into the input 
+
+	console.log("Input is equal to:" + input);
+
+      	if 	(input == ''){
+
+      		// This "set's it off." if there is nothing
+      		// entered into the input field the audio file will play
+
+      		// console.log("Input.text is equal to:" + input);
+      			
+      		var audio = $('#nedry')[0];
+			audio.play()
+			// This will play the audio and is connected to the
+			// ID of the audio file in your HTML
+      	
+      	// ********************************************************
+      	// Image
+      	
+    // Animation complete
+    $('.theGuy').fadeIn('slow', function(){
+    	('.theGuy').delay(5000).fadeOut('slow');
+    });
+
+
+
+
+
+// console.log('no info');
 
 			
-	var input= $(this).find('input[type=search]')
-
-      	if 	(input.text() ==''){
-
-      		console.log('no info')
-      	};
-
 		
-	})  
-      
+
+      	};  // End of if statement
+
+     
+		
+	})   // End of this.on('submit')
+     
+
+};    // end of fn.WrongAnswer
 
 
-};
 
 
 
 
- // function play(){
- //       var audio = document.getElementById("audio");
- //       audio.play();
- //                 }
 
 
- // .alertSound.play('audio.src')
+
+      		
+      			
